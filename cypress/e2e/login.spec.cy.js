3299eec3dffd0f316c3f899a54e07f91d2d1b3a1
PauloEspinoza <paulohenrique.espinoza@gmail.com>
2024-10-23 17:56:55 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,7 +11,7 @@ describe('Orange HRM Tests', () => {
   }
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).should('be.visible').type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).should('be.visible').type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).should('be.visible').click('center')
@@ -20,7 +20,7 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).should('be.visible').type(userData.userFail.username)
     cy.get(selectorsList.passwordField).should('be.visible').type(userData.userFail.password)
     cy.get(selectorsList.loginButton).should('be.visible').click('center')
