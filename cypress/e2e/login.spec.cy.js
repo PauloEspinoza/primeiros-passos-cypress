ec0da055d8614bdb731ea837156ea9f997c30066
PauloEspinoza <paulohenrique.espinoza@gmail.com>
2024-10-23 17:44:05 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,26 +1,28 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']",
   }
 
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).should('be.visible').type('Admin')
-    cy.get(selectorsList.passwordField).should('be.visible').type('admin123')
+    cy.get(selectorsList.usernameField).should('be.visible').type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).should('be.visible').type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).should('be.visible').click('center')
     cy.location('pathname').should('be.equal','/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).should('be.visible').contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid).should('be.visible')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).should('be.visible').type('Qwerty')
-    cy.get(selectorsList.passwordField).should('be.visible').type('admin123')
+    cy.get(selectorsList.usernameField).should('be.visible').type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).should('be.visible').type(userData.userFail.password)
     cy.get(selectorsList.loginButton).should('be.visible').click('center')
     cy.get(selectorsList.wrongCredentialAlert).should('be.visible')
   })
